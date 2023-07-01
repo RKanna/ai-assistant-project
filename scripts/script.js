f179d2dc56e7f33ba53e9f2c0520d2088cd14f4b
RKanna <rkannan.balakrishnan@hotmail.com>
2023-07-02 03:45:48 +0530

    scripts for slabs and click of felix

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+window.addEventListener("click", function () {
+  let aiAssistant = document.getElementById("ai-assistant");
+  aiAssistant.style.backgroundColor = "#22bf99";
+  aiAssistant.classList.add("jump");
+
+  setTimeout(function () {
+    aiAssistant.style.backgroundColor = "";
+    aiAssistant.classList.remove("jump");
+  }, 10000);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slabs = document.querySelectorAll(
+    ".behind-slab, .center-slab, .front-slab"
+  );
+
+  slabs.forEach((slab) => {
+    slab.style.display = "none";
+  });
+});
+
+window.addEventListener("click", function () {
+  const slabs = document.querySelectorAll(
+    ".behind-slab, .center-slab, .front-slab"
+  );
+
+  slabs.forEach((slab) => {
+    slab.classList.add("transition-effect");
+    slab.style.display = "flex";
+  });
+});
